ef1bb0441c0b9f2a3818a1cdeb412b8f9b3aef18
=Mano <=Mano@xyz.com>
2023-10-04 23:07:05 -0500

    Commiting Selenium Files

---
+++ src/main/java/com/demo/selenium_automation/App.java
@@ -0,0 +1,68 @@
+package com.demo.selenium_automation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        // Initialize the driver property
+    	
+    	System.out.println("Test started");
+    	
+    	System.setProperty("webdriver.chrome.driver","C:\\Users\\Manoj Shanmugham\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+    	
+    	//setup chrome parameters
+    	
+    	ChromeOptions chromeOptions = new ChromeOptions();
+    	//chromeOptions.addArguments("--headless");
+    	
+    	//Initialize the driver
+    	
+    	WebDriver driver = new ChromeDriver(chromeOptions);
+    	
+    	//Invoke wait to load the application
+    	
+    	driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+    	
+    	//open the url in the browser
+    	driver.get("https://www.facebook.com/");
+    	
+    	
+    	//pass xyz as an email id
+    	driver.findElement(By.id("email")).sendKeys("xyz");
+    	
+    	//sleep times to check the flow
+    	Thread.sleep(1000);
+    	
+    	//pass abc as password
+    	driver.findElement(By.id("pass")).sendKeys("abc");
+    	
+    	
+    	Thread.sleep(1000);
+    	      	   	
+    	
+    	driver.findElement(By.name("login")).click();
+    	
+    	Thread.sleep(10000);
+    	
+    	
+    	System.out.println("Test Done");
+    	
+    	
+    	
+    	driver.close();
+    	
+    	
+    	
+    }
+}
